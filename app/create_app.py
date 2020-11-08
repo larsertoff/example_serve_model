ae8f17759578802baa5eed7b2f8f71e4af2f585e
larslehrem <lars.lovberg@gmail.com>
2020-11-09 00:00:25 +0100

    See if textblob needs something on azure

--- app/create_app.py
+++ app/create_app.py
@@ -8,9 +8,11 @@ def create_app(config_name= 'dev_config.py'):
     app = flask.Flask(__name__)
 
     app.config['SECRET_KEY'] = '123456'
-    # Split this out as blueprints if needed
+    # I only need one simple route for this for now
     @app.route('/')
     def before_doing_anything():
-        return 'Before doing anything'
+        test = textblob.TextBlob("Textblob is amazingly simple to use. What great fun!")
+        print(test.sentiment)
+        return str(test.sentiment)
 
     return app
