f91a9c02181a9dfef4987709b454f7fee49cbb9d
larslehrem <lars.lovberg@gmail.com>
2020-11-10 23:43:31 +0100

    Now I mean business

--- config/prod_config.py
+++ config/prod_config.py
@@ -1,5 +1,4 @@
 FLASK_ENV = 'production'
 SECRET_KEY = 'for dev only'
 SESSION_TYPE = 'filesystem'
-SESSION_TYPE = 'filesystem'
 CONNECTION_STRING = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:examplemodelsqlserver.database.windows.net,1433;Database=examplemodeldb;Uid=larslehre;Pwd=I<3finance91;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
