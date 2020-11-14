016043cd1f31263ce821e861b3dd70d091b6adcf
larslehrem <lars.lovberg@gmail.com>
2020-11-15 00:46:51 +0100

    Final push for now

--- config/dev_config.py
+++ config/dev_config.py
@@ -3,4 +3,4 @@ FLASK_ENV = 'development'
 SECRET_KEY = 'thisisakeythatmakesiswork'
 TESTING = True
 SESSION_TYPE = 'filesystem'
-CONNECTION_STRING = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:examplemodelsqlserver.database.windows.net,1433;Database=examplemodeldb;Uid=larslehre;Pwd=I<3finance91;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+WRITE_RESULTS = False
